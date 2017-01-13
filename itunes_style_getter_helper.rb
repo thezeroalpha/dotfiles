7d0f91021d704336d7c3514242695dc5b55b5ba9
Alex Balgavy <a.balgavy@gmail.com>
2017-01-13 21:37:32 +0100

    Added more options to itunes_style_getter
    I can now select from more than one style

--- itunes_style_getter_helper.rb
+++ itunes_style_getter_helper.rb
@@ -2,12 +2,12 @@ require "discogs-wrapper"
 
 my_user_token = ENV["DISCOGS_API_TOKEN"]
 
-query="#{ARGV[0]} #{ARGV[1]}"
+query="#{ARGV[0]} #{ARGV[1]} #{ARGV[2]}"
 wrapper=Discogs::Wrapper.new("iTunesStyleGetter", user_token: my_user_token)
 search=wrapper.search("#{query}")
 search.results.each do |result|
-  if result.style?
-    puts result.style[0]
-    break
+  if result.style? && !result.style.empty?
+    p result.style.to_a
+    exit
   end
 end
