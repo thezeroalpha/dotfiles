7ade09c51dc69df1e520be2082d55b64534df0d7
Alex Balgavy <a.balgavy@gmail.com>
2017-01-13 18:13:11 +0100

    Added in the possibility of manual override to itunes_style_getter
    If you don't like what the program found, you can enter your own genre

--- itunes_style_getter_helper.rb
+++ itunes_style_getter_helper.rb
@@ -1,7 +1,9 @@
 require "discogs-wrapper"
 
+my_user_token = ENV["DISCOGS_API_TOKEN"]
+
 query="#{ARGV[0]} #{ARGV[1]}"
-wrapper=Discogs::Wrapper.new("iTunesStyleGetter", user_token: "uTZcIvlAFxZzteceXTKJlZLRgSJhbiJCOBIjZIbx")
+wrapper=Discogs::Wrapper.new("iTunesStyleGetter", user_token: my_user_token)
 search=wrapper.search("#{query}")
 search.results.each do |result|
   if result.style?
