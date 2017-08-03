bc92e4e13a7f3f7684295e8ea4b845ab7e214e6f
Alex Balgavy <a.balgavy@gmail.com>
2017-08-03 16:40:22 +0200

    Modifications

--- itunes_style_getter_helper.rb
+++ itunes_style_getter_helper.rb
@@ -2,14 +2,24 @@ require "discogs-wrapper"
 
 my_user_token = ENV["DISCOGS_API_TOKEN"]
 
-query="#{ARGV[0]} #{ARGV[1]} #{ARGV[2]}"
+
+query=ARGV.join(" ")
 wrapper=Discogs::Wrapper.new("iTunesStyleGetter", user_token: my_user_token)
+res=[]
 search=wrapper.search("#{query}")
 search.results.each do |result|
   if result.style? && !result.style.empty?
-    p result.style.to_a
+    res="http://discogs.com#{result.uri}".split(" ")
+    result.style.to_a.each {|x| res.push x }
+    break
+  end
+end
+search.results.each do |result|
+  if result.genre? && !result.genre.empty?
+    result.genre.each do |g|
+      res.push("#{g}")
+    end
+    p res
     exit
   end
 end
-
-p search["results"][0].genre[0].split(",")
