ea5c8031e32229d345166d57a600c7a1b1179577
Alex Balgavy <a.balgavy@gmail.com>
2017-08-04 18:10:05 +0200

    iTunes Style Getter improvements
    Changed it to write info in the format "Genre/Style". Added comments. Also, split up the genre, style, and URL queries into three separate parts, using flags (arguments to the Ruby script) to switch between them.
    
    To query for a genre, use the 'g' flag. For a style, use the 's' flag. For a URL, use any or no flag.

--- itunes_style_getter_helper.rb
+++ itunes_style_getter_helper.rb
@@ -1,25 +1,40 @@
-require "discogs-wrapper"
+require "discogs-wrapper" # Needed for API access
 
+# Remove this if sharing. This is required and unique for each user.
 my_user_token = ENV["DISCOGS_API_TOKEN"]
 
+flag=ARGV.shift # Whether searching for genre or style
+query=ARGV.join(" ") # Join the rest of the arguments into a search string
 
-query=ARGV.join(" ")
+# Search, and wrap the results in an array
 wrapper=Discogs::Wrapper.new("iTunesStyleGetter", user_token: my_user_token)
 res=[]
 search=wrapper.search("#{query}")
-search.results.each do |result|
-  if result.style? && !result.style.empty?
-    res="http://discogs.com#{result.uri}".split(" ")
-    result.style.to_a.each {|x| res.push x }
-    break
+
+# If searching for a genre
+if (flag=="g")
+  search.results.each do |result|
+    if result.genre? && !result.genre.empty?
+      result.genre.each do |g|
+        res.push("#{g}")
+      end
+    p res
   end
 end
-search.results.each do |result|
-  if result.genre? && !result.genre.empty?
-    result.genre.each do |g|
-      res.push("#{g}")
+# If searching for a style
+elsif (flag=="s")
+  search.results.each do |result|
+    if result.style? && !result.style.empty?
+      result.style.to_a.each {|x| res.push x}
     end
     p res
-    exit
+  end
+# If searching for url
+else
+  search.results.each do |result|
+    if result.style? && !result.style.empty?
+      res="http://discogs.com#{result.uri}".split(" ")
+    end
+    p res
   end
 end
