b2a065debb20b9682d9840f850a9434f45652165
Alex Balgavy <a.balgavy@gmail.com>
2018-05-17 23:15:44 +0200

    Removed unneeded hidden files

--- .bin/modified_cfscrape.py
+++
@@ -1,6 +0,0 @@
-import cfscrape, sys
-scraper=cfscrape.create_scraper()
-url=sys.argv[1]
-cfurl = scraper.get(url).content
-name = url.split('/')[-1].replace("$20", "")
-with open(name, 'wb') as f: f.write(cfurl)
