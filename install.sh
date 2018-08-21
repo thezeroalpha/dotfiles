803f9eab85d7d4a3095a7aa26146ffbe6edaddf0
Unknown <a.balgavy@gmail.com>
2018-08-21 13:11:22 +0200

    Did some reorganisation

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-
-for i in *; do
+echo "Setting up your Mac..."
+if ! command -v brew &> /dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+brew bundle
+for i in $(ls dotfiles); do
     case "$i" in
         "vim-config")
-            echo "ln -shf $(pwd)\"$i\" ~/.config/\"$i\""
-            ln -shf $(pwd)/"$i" ~/.config/"$i"
-            ;;
-        "exclude")
+            ln -shfv $(pwd)/dotfiles/"$i" ~/.config/"$i"
             ;;
         *)
-            echo "ln -shf $(pwd)/$i ~/.$i"
-            ln -shf $(pwd)/$i ~/.$i
+            ln -shfv $(pwd)/dotfiles/$i ~/.$i
             ;;
     esac
 done
-ln -shf ~/.config/vim-config/init.vimrc ~/.vimrc
+ln -shfv ~/.config/vim-config/init.vimrc ~/.vimrc
