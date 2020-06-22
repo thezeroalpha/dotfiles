2a92456f2119de4d65500baf10ae352d235cb776
Alex Balgavy <a.balgavy@gmail.com>
2020-06-22 12:05:11 +0200

    shell: make ^P/^N fuzzy search history like up/down
    Former-commit-id: f386afda141623af4a71d7cd4a4ee245c6cad64c

--- shell/oh-my-zsh-defaults/keys.zsh
+++ shell/oh-my-zsh-defaults/keys.zsh
@@ -22,12 +22,14 @@ if [[ "${terminfo[kcuu1]}" != "" ]]; then
   autoload -U up-line-or-beginning-search
   zle -N up-line-or-beginning-search
   bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+  bindkey "^P" up-line-or-beginning-search
 fi
 # start typing + [Down-Arrow] - fuzzy find history backward
 if [[ "${terminfo[kcud1]}" != "" ]]; then
   autoload -U down-line-or-beginning-search
   zle -N down-line-or-beginning-search
   bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+  bindkey "^N" down-line-or-beginning-search
 fi
 
 if [[ "${terminfo[khome]}" != "" ]]; then
