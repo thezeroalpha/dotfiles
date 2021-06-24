3bc37907b6099c21f01552621cad206a412e5d09
Alex Balgavy <alex@balgavy.eu>
2021-06-24 21:33:25 +0200

    emacs: relative line numbers

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,6 @@
+;; Follow symlinks without prompting (the org file is a symlink)
+(setq vc-follow-symlinks t)
+
 (org-babel-load-file
  (expand-file-name "config.org" user-emacs-directory))
 
@@ -7,7 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(doom-themes all-the-icons use-package-ensure which-key use-package org-bullets helm exec-path-from-shell)))
+   '(anki-connect anki-editor doom-themes all-the-icons use-package-ensure which-key use-package org-bullets helm exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
