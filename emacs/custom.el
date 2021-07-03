8734af60533610ee27f496da02bbe01d28531729
Alex Balgavy <alex@balgavy.eu>
2021-07-03 19:37:53 +0200

    emacs: add some settings from better-defaults
    This repo here https://git.sr.ht/~technomancy/better-defaults

---
+++ emacs/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "e3c64e88fec56f86b49dcdc5a831e96782baf14b09397d4057156b17062a8848" "f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" default))
+ '(package-hidden-regexps '("^I"))
+ '(package-selected-packages
+   '(vterm notmuch magit lean-mode markdown-mode anki-connect anki-editor doom-themes all-the-icons use-package-ensure which-key use-package org-bullets helm exec-path-from-shell)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-buffer-file ((t (:inherit default))))
+ '(helm-ff-file-extension ((t (:inherit default))))
+ '(helm-non-file-buffer ((t (:inherit font-lock-comment-face))))
+ '(pulse-highlight-start-face ((t (:background "CadetBlue2")))))
