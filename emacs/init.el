0f82f3827f12369d9467997fd85684975b6dbc7a
Alex Balgavy <alex@balgavy.eu>
2021-07-15 11:05:47 +0200

    emacs: move GUI customizations into init.el
    Bars should be hidden and frame should be fullscreen as the very first
    thing.

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,17 @@
 ;; Follow symlinks without prompting (the org file is a symlink)
 (setq vc-follow-symlinks t)
 
+;; Get rid of all bars
+(setq org-src-tab-acts-natively t)
+(when window-system
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tooltip-mode -1))
+
+;; Start in fullscreen mode
+(toggle-frame-fullscreen)
+
+;; Load all other customization from the org file
 (org-babel-load-file
  (expand-file-name "config.org" user-emacs-directory))
