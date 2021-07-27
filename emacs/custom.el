ab2e3b49d5e6eac279091b75f9bd2f0e34e98f97
Alex Balgavy <alex@balgavy.eu>
2021-07-27 11:58:55 +0200

    emacs: fix semantic header line color

--- emacs/custom.el
+++ emacs/custom.el
@@ -14,6 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(header-line ((t (:background "gray87"))))
  '(helm-buffer-file ((t (:inherit default))))
  '(helm-ff-file-extension ((t (:inherit default))))
  '(helm-non-file-buffer ((t (:inherit font-lock-comment-face))))
