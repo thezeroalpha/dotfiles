82292e09fcc3b4d0b893ecf5bf424968f5d93bf8
Alex Balgavy <alex@balgavy.eu>
2022-11-19 19:24:59 +0100

    nvim: config

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,9 @@ let &packpath = &runtimepath
 source ~/.vim/vimrc
 ]])
 
+-- neovim plugins
+require('plugins')
+
 local custom_lsp_attach = function(client)
   -- See `:help nvim_buf_set_keymap()` for more information
   vim.api.nvim_buf_set_keymap(0, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap = true})
@@ -22,3 +25,34 @@ end
 require'lspconfig'.rust_analyzer.setup({
   on_attach = custom_lsp_attach
 })
+require'lspconfig'.pyright.setup{}
+
+require'nvim-treesitter.configs'.setup {
+  -- Automatically install missing parsers when entering buffer
+  auto_install = true,
+  -- Consistent syntax highlighting
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
+  }
+}
+vim.cmd([[
+set foldmethod=expr
+set foldexpr=nvim_treesitter#foldexpr()
+]])
