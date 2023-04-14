3f8f52eb5cde4184aaa604519287dbc09c99f878
Alex Balgavy <alex@balgavy.eu>
2023-04-14 15:48:35 +0200

    nvim: update null_ls

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,7 +31,7 @@ return {
     'jose-elias-alvarez/null-ls.nvim',
     config = function()
       local null_ls = require('null-ls')
-      null_ls.setup { sources = { null_ls.builtins.code_actions.shellcheck } }
+      null_ls.setup { sources = { null_ls.builtins.code_actions.shellcheck, null_ls.builtins.diagnostics.checkmake } }
     end,
   },
 
