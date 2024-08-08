ff099884fa266c1b3a62ac46f41f834c957e4dc7
Alex Balgavy <alexander.balgavy@spaceapplications.com>
2024-08-08 16:30:47 +0200

    nvim: configs

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -9,8 +9,8 @@ require('telescope').setup {
     },
   },
 }
-vim.keymap.set('n', '<leader>sp', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>sp', require('telescope.builtin').oldfiles, { desc = '[S]earch [p]reviously opened files' })
+vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing buffers' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>cv', function() require('telescope.builtin').find_files({ cwd = vim.env.DOTFILES..'/nvim' }) end, { desc = 'Neovim configs' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
@@ -18,12 +18,12 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').lsp_references, { desc = '[S]earch [R]eferences' })
-vim.keymap.set('n', '<leader>sI', require('telescope.builtin').lsp_incoming_calls, { desc = '[S]earch [I]ncoming calls' })
-vim.keymap.set('n', '<leader>sO', require('telescope.builtin').lsp_outgoing_calls, { desc = '[S]earch [O]utgoing calls' })
-vim.keymap.set('n', '<leader>si', require('telescope.builtin').lsp_implementations, { desc = '[S]earch [i]mplementations' })
+vim.keymap.set('n', '<leader>s<', require('telescope.builtin').lsp_incoming_calls, { desc = '[S]earch [I]ncoming calls' })
+vim.keymap.set('n', '<leader>s>', require('telescope.builtin').lsp_outgoing_calls, { desc = '[S]earch [O]utgoing calls' })
+vim.keymap.set('n', '<leader>sI', require('telescope.builtin').lsp_implementations, { desc = '[S]earch [i]mplementations' })
 vim.keymap.set('n', '<leader>ss', function()
-  local ok, lsp_buf = pcall(require, 'vim.lsp.buf')
-  if ok and lsp_buf.server_ready() then
+  local server_ready = not not vim.lsp.buf_notify(0, '$/progress', {})
+  if server_ready then
     require('telescope.builtin').lsp_dynamic_workspace_symbols()
   else
     require('telescope.builtin').treesitter()
@@ -31,8 +31,8 @@ vim.keymap.set('n', '<leader>ss', function()
 end, { desc = '[S]earch [S]ymbols' })
 vim.keymap.set('n', '<leader>sj', require('telescope.builtin').jumplist, { desc = '[S]earch [J]umplist' })
 vim.keymap.set('n', '<leader>sb', function()
-  local ok, lsp_buf = pcall(require, 'vim.lsp.buf')
-  if ok and lsp_buf.server_ready() then
+  local server_ready = not not vim.lsp.buf_notify(0, '$/progress', {})
+  if server_ready then
     require('telescope.builtin').lsp_document_symbols()
   else
     require('telescope.builtin').tags()
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -213,10 +213,6 @@ return {
     require("nvim-dap-virtual-text").setup()
   end},
 
-  { 'lvimuser/lsp-inlayhints.nvim', config = function()
-    require 'config.lsp-inlayhints'
-  end},
-
   { 'Eandrju/cellular-automaton.nvim', config = function()
     vim.keymap.set('n', 'q:', '<cmd>CellularAutomaton make_it_rain<CR>')
   end},
@@ -332,5 +328,29 @@ return {
   {
     "echasnovski/mini.align",
     config = function(_, opts) require'mini.align'.setup() end,
+  },
+  {
+    "m00qek/baleia.nvim",
+    version = "*",
+    config = function()
+      vim.g.baleia = require("baleia").setup({ })
+
+      -- Command to colorize the current buffer
+      vim.api.nvim_create_user_command("BaleiaColorize", function()
+        vim.g.baleia.once(vim.api.nvim_get_current_buf())
+      end, { bang = true })
+
+      -- Command to show logs
+      vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+    end,
+  },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Not needed, already handled by plugin
+
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    }
   },
 }
