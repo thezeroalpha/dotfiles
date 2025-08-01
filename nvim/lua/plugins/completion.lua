e56a849d8cf9d0ee19df3eac191ad80c55e7da90
Alex Balgavy <alexander.balgavy@spaceapplications.com>
2025-08-01 15:45:28 +0200

    nvim: disable cmdline completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -30,7 +30,11 @@ return {
     },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = false } },
+    completion = {
+      documentation = { auto_show = false },
+    },
+
+    cmdline = { enabled = false },
 
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
