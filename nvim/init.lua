9861594ba08f83f3483583802be4627811c7aa4c
Alex Balgavy <alex@balgavy.eu>
2025-08-31 15:20:04 +0200

    emacs: org-mode config

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,32 +9,32 @@ source $MYOLDVIMRC
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.g.mapleader = " "
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Install Lazy
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load everything from ./lua/plugins.lua, ./lua/plugins/*.lua
-require('lazy').setup('plugins', {
-  performance = {
-    rtp = {
-      reset = false,
-    },
-  },
+require("lazy").setup("plugins", {
+	performance = {
+		rtp = {
+			reset = false,
+		},
+	},
 })
 
 -- Set completeopt to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
+vim.o.completeopt = "menuone,noselect"
 
-require('highlight_on_yank')
-require('mappings')
-require('netrw_target')
+require("highlight_on_yank")
+require("mappings")
+require("netrw_target")
